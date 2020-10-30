8845596047e2cc6589d4ab38d04d970ee92b3d8a
RuachOdile <72966621+RuachOdile@users.noreply.github.com>
2020-10-30 19:46:50 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,32 @@
+
+
+
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the first number");
+		Scanner sc= new Scanner (System.in);
+		int num1 =sc.nextInt();
+		System.out.println("Enter the second number");
+		int num2=sc.nextInt();
+		
+		int sum=num1+num2;
+		System.out.println("Sum of the numbers is :: "+sum);
+	
+		
+		
+
+	}
+
+}
+
+OUTPUT
+*********************************
+Enter the first number
+12
+Enter the second number
+12
+Sum of the numbers is :: 24
