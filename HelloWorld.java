d5b28df9dd8bb7934cbd44ffde1b881e0dde98f1
RuachOdile <72966621+RuachOdile@users.noreply.github.com>
2020-10-30 19:45:36 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class PrintHelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+	}
+
+}
+
+Output
+****************
+hello world
